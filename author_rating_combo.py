4bad75c4548aee71e72c08e32f0c940608969c43
S-acharya57 <acharyasa38@gmail.com>
2023-04-28 13:42:52 +0545

    First commit for adding simple recommendations with genre clustering, authors and rating as well as based on books description.

---
+++ author_rating_combo.py
@@ -0,0 +1,74 @@
+"""
+This is responsible for just suggesting books with same authors in the order of book_ratings.
+A book can have multiple authors. Thus, it includes all the books having similar authors but in chronological
+order of books based on book_rating column of the excel sheet.
+"""
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv("dataset/book_data.csv")
+df.fillna(value="", inplace=True)
+
+
+df.drop_duplicates(subset=["book_title"], inplace=True)
+
+
+df["genres"] = df["genres"].apply(lambda x: x.split("|"))
+
+df["book_authors"] = df["book_authors"].apply(lambda x: x.split("|"))
+
+df["book_pages"] = pd.to_numeric(
+    df["book_pages"].str.replace(" pages", ""), errors="coerce"
+)
+
+df["book_pages"] = (
+    df["book_pages"]
+    .astype(str)
+    .str.replace(" pages", "")
+    .replace("", np.nan)
+    .astype(float)
+)
+
+# print(df.describe())
+# print(df.info())
+# print(df.info)
+authors = [author for authors in df["book_authors"] for author in authors]
+unique_authors = list(set(authors))
+
+le = LabelEncoder()
+
+author_labels = le.fit_transform(unique_authors)
+author_dict = dict(zip(unique_authors, author_labels))
+
+# Create a new column in the dataframe with the integer labels for each author
+df["author_labels"] = [
+    list(map(lambda x: author_dict[x], authors)) for authors in df["book_authors"]
+]
+
+# print(df["author_labels"])
+
+
+def recommend(inp_book_title, df_all):
+    book_index = df_all.index[df["book_title"] == inp_book_title].tolist()[0]
+    # print(f'Book index is {book_index}')
+
+    book_authors = df.loc[book_index, "author_labels"]
+    # print(f'Book authors are {book_authors}')
+
+    # gives top rated books in order of the same authors
+    recommended_books = df[
+        df["author_labels"].apply(lambda x: set(x) == set(book_authors))
+    ].sort_values("book_rating")
+    # print(recommended_books)
+
+    if book_index in recommended_books.index:
+        recommended_books = recommended_books.drop(book_index)
+
+    return recommended_books
+
+
+book_name = input("Enter the name of a book")
+books = recommend(book_name, df)
+print(books["book_title"])
---
+++ content_based_filtering.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+
+df = pd.read_csv("dataset/book_data.csv")
+
+
+df.fillna(value="", inplace=True)
+df.drop_duplicates(subset=["book_title"], inplace=True)
+
+
+df["genres"] = df["genres"].apply(lambda x: x.split("|"))
+
+
+df["book_authors"] = df["book_authors"].apply(lambda x: x.split("|"))
+
+
+# print(df["book_pages"].unique())
+df["book_pages"] = pd.to_numeric(
+    df["book_pages"].str.replace(" pages", ""), errors="coerce"
+)
+
+
+df["book_pages"] = (
+    df["book_pages"]
+    .astype(str)
+    .str.replace(" pages", "")
+    .replace("", np.nan)
+    .astype(float)
+)
+print(df["book_title"].count())
+
+
+vectorizer = TfidfVectorizer(stop_words="english")
+
+books_desc_vector = vectorizer.fit_transform(df["book_desc"].values.astype(str))
+
+print(f"\nBook description vector shape is {books_desc_vector.shape}\n")
+
+all_in_vectors = vectorizer.get_feature_names_out()
+
+cos_similarities = cosine_similarity(books_desc_vector)
+print(f"Cosine similarities shape {cos_similarities.shape}\n")
+
+
+df2 = df[["book_title", "book_authors", "book_desc"]]
+
+
+indices = pd.Series(df2.index, index=df["book_title"]).drop_duplicates()
+
+
+def give_recommendation(title, cos_similarities=cos_similarities):
+    book_index = indices[title]
+    similarity_scores = list(enumerate(cos_similarities[book_index]))
+    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+    top_books = [i[0] for i in similarity_scores[1:5]]
+    recommended_books = df2.iloc[top_books]["book_title"].tolist()
+    return recommended_books
+
+
+import string
+
+df["name_feature"] = ["name_{}".format(x) for x in df["book_title"]]
+
+
+def raw_text_to_feature(
+    s,
+    sep=" ",
+    join_sep="x",
+    to_include=string.ascii_lowercase,
+    to_include2=string.ascii_uppercase,
+):
+    def filter_word(word):
+        return "".join([c for c in word if (c in to_include or to_include2)])
+
+    return join_sep.join([filter_word(word) for word in s.split(sep)])
+
+
+df["name_feature"] = df["book_title"].apply(raw_text_to_feature)
+
+
+df["corpus"] = pd.Series(
+    df[["book_desc", "name_feature"]].fillna("").values.tolist()
+).str.join(" ")
+df["corpus"] = df["corpus"].fillna("")
+
+
+print(
+    f'Corpus count -> {df["corpus"].count()}, Name feature count -> {df["name_feature"].count()}'
+)
+
+
+vectorizer2 = TfidfVectorizer(stop_words="english")
+books_desc_vector2 = vectorizer.fit_transform(df["corpus"].values.astype(str))
+all_in_vectors2 = vectorizer.get_feature_names_out()
+
+cos_similarities2 = cosine_similarity(books_desc_vector2)
+print(type(cos_similarities2))
+np.save("model_pickles/similarity_scores_genre_description.npy", cos_similarities2)
+temp = np.load("model_pickles/similarity_scores_genre_description.npy")
+print(temp == cos_similarities2)
+"""
+
+with open("model_pickles/similarity_scores_genre_description.pkl", "wb") as f:
+    pickle.dump(cos_similarities2, f)
+    print("Pickle dumped for genre description similarity scores")
+"""
+
+
+def give_recommendation_name_desc(title, cos_similarities=cos_similarities2):
+    book_index = indices[title]
+    similarity_scores = list(enumerate(cos_similarities[book_index]))
+    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+    print(similarity_scores)
+    top_books = [i[0] for i in similarity_scores[:10]]
+    recommended_books = [df2.iloc[top_books]["book_title"].tolist()]
+    print(type(recommended_books[0]))
+    res = []
+    for i, book in enumerate(recommended_books[0]):
+        res.append((book, similarity_scores[i][-1]))
+        print(res[-1])
+    return res
+
+
+# book_name = input("Enter any name of the books\n")
+# recs2 = give_recommendation_name_desc(book_name)
